9a371a9f85619566ad91632218c08f1f1797c09d
FeLipe-133 <fcaroyar@umag.cl>
2024-11-21 19:18:33 -0300

    añadiendo algunas tablas

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey,  DateTime, Text
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -7,3 +7,28 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class Project(Base):
+    __tablename__ = 'projects'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    description = Column(Text, nullable=False)
+    goal_amount = Column(Float, nullable=False)
+    current_amount = Column(Float, default=0.0)
+    creator_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+
+    creator = relationship("User", back_populates="projects")
+
+class Contribution(Base):
+    __tablename__ = 'contributions'
+    id = Column(Integer, primary_key=True, index=True)
+    project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    amount = Column(Float, nullable=False)
+    contributed_at = Column(DateTime, nullable=False)
+
+    user = relationship("User")
+    project = relationship("Project")
