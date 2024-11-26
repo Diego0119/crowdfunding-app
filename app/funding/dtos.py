c06eaf63661495fa121127e712c9b7588ef22772
FeLipe-133 <fcaroyar@umag.cl>
2024-11-26 17:44:39 -0300

    update dtos proyecto

--- app/funding/dtos.py
+++ app/funding/dtos.py
@@ -1,2 +1,38 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
+from datetime import datetime
+
+#dtos para el proyecto
+class ProjectBase(BaseModel):
+    name: str 
+    description: str  
+    goal_amount: float 
+    start_date: datetime  
+    end_date: datetime 
+    category: str 
+    rewards: str  
+
+class ProjectCreate(ProjectBase):
+    creator_id: int  
+
+
+class ProjectUpdate(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    goal_amount: Optional[float]
+    start_date: Optional[datetime]
+    end_date: Optional[datetime]
+    category: Optional[str]
+    rewards: Optional[str]
+    status: Optional[str]  
+
+
+class ProjectOut(ProjectBase):
+    id: int 
+    contributions_count: int  
+    current_amount: float  
+    status: str  
+
+    class Config:
+        orm_mode = True  
+
