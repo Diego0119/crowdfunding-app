f257a21707330cd1f9fa1b288bf8047900b03484
FeLipe-133 <fcaroyar@umag.cl>
2024-11-26 16:51:07 -0300

    update

--- app/funding/models.py
+++ app/funding/models.py
@@ -2,19 +2,6 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey,  DateTi
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-
-    projects_created = Column(Integer, default=0)
-    projects_contributed = Column(Integer, default=0)
-
-    projects = relationship("Project", back_populates="creator")
-    contributions = relationship("Contribution", back_populates="user")
-    
 class Project(Base):
     __tablename__ = 'projects'
     id = Column(Integer, primary_key=True, index=True)
